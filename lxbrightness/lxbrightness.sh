86dbee63ae4499d13974d4da65af795f707dd2e1
Gus Gómez <gusgomez90@proton.me>
2025-01-25 11:50:07 -0600

    Add files via upload

--- lxbrightness/lxbrightness.sh
+++ lxbrightness/lxbrightness.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 
-# Obtener el nombre de la salida de la pantalla conectada
+config_file="$HOME/.ajustar_brillo_config"
+
+select_language() {
+    lang=$(zenity --list --title="Select Language" --column="Language" "English" "Español")
+    if [ "$lang" == "English" ]; then
+        echo "en" > "$config_file"
+    elif [ "$lang" == "Español" ]; then
+        echo "es" > "$config_file"
+    else
+        exit 1
+    fi
+}
+
+if [ -f "$config_file" ]; then
+    language=$(cat "$config_file")
+else
+    language=$(select_language)
+fi
+
+if [ "$language" == "en" ]; then
+    title="Adjust Brightness"
+    prompt="Enter a brightness value (0.2 - 1.0):"
+    error_range="Please enter a value between 0.2 and 1.0."
+    error_valid="Please enter a valid value between 0.2 and 1.0."
+else
+    title="Ajustar Brillo"
+    prompt="Ingrese un valor de brillo (0.2 - 1.0):"
+    error_range="Por favor, ingrese un valor entre 0.2 y 1.0."
+    error_valid="Por favor, ingrese un valor válido entre 0.2 y 1.0."
+fi
+
 output=$(xrandr --query | grep " connected" | awk '{ print $1 }')
 
-# Función para ajustar el brillo
 set_brightness() {
     xrandr --output "$output" --brightness "$1"
 }
 
-# Bucle para permitir al usuario ingresar el brillo
 while true; do
-    # Pedir al usuario que ingrese un valor de brillo
-    brightness=$(zenity --entry --title="Ajustar Brillo" --text="Ingrese un valor de brillo (0.2 - 1.0):")
+    brightness=$(zenity --entry --title="$title" --text="$prompt")
 
-    # Verificar si el usuario canceló
     if [ $? -ne 0 ]; then
         break
     fi
@@ -23,9 +49,9 @@ while true; do
         if (( $(echo "$brightness >= 0.2" | bc -l) && $(echo "$brightness <= 1.0" | bc -l) )); then
             set_brightness "$brightness"
         else
-            zenity --error --text="Por favor, ingrese un valor entre 0.2 y 1.0."
+            zenity --error --text="$error_range"
         fi
     else
-        zenity --error --text="Por favor, ingrese un valor válido entre 0.2 y 1.0."
+        zenity --error --text="$error_valid"
     fi
 done
